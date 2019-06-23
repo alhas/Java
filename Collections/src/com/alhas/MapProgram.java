3b49dc9b5e71b74caaff144b1cdc395de7849177
baalhas <bahtiyaralialhas@gmail.com>
2019-06-24 01:27:27 +0200

    Map Section End

--- Collections/src/com/alhas/MapProgram.java
+++ Collections/src/com/alhas/MapProgram.java
@@ -27,11 +27,39 @@ public class MapProgram {
         }
         System.out.println(languages.put("Java", "this course us about Java"));
 
+      //  languages.remove("Lisp");
+
+
+
 
         System.out.println("============================================================");
+
+
+
+        if(languages.remove("Algol","an algorithmic language")){
+            System.out.println("Algol removed");
+        }else{
+            System.out.println("Algol not removed, key/value pair not found.");
+        }
+
+        if(languages.replace("Lisp","Therein lies madness","Functional programing languages with imperative features")){
+            System.out.println("Lisp replaced");
+        }else{
+            System.out.println("Lisp was not replaced.");
+        }
+
+
+        //System.out.println(languages.replace("Scala","this will not be added"));
+
+
+
+
         for (String key : languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
         }
+
+
+
 
     }
 
